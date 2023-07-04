aab2ed848b67f7d1a8bf2dd2ea34dd32d5b5de42
Mathieu Gerard <mathieu2991@gmail.com>
2023-07-04 15:06:59 +0200

    ajout de la partie connection

---
+++ login.php
@@ -0,0 +1,54 @@
+<?php
+    include_once('connection.php');
+
+    $query = 'SELECT * FROM users';
+    $userStatement = $baseQuizz -> prepare($query);
+    $userStatement -> execute();
+    $users = $userStatement -> fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
+    if (isset($_POST['pseudo'])) {
+        foreach ($users as $user) {
+        if ($user['pseudo'] === $_POST['pseudo']) {
+            $loggedUser = [
+                'pseudo' => $user['pseudo']
+            ];
+
+                $_SESSION['LOGGED_USER'] = $loggedUser['pseudo'];
+            
+        } else {
+            $name = htmlspecialchars($_POST['pseudo']);
+            $avatar = 'images/pikachu.png';
+            $insertStatement = $baseQuizz -> prepare("INSERT INTO users (pseudo, avatar) VALUES (:pseudo, :avatar)");
+            $insertStatement -> execute([
+                'pseudo' => $name,
+                'avatar' => $avatar
+            ]);
+        }
+    }}
+?>
+
+<?php
+if (isset($_SESSION['LOGGED_USER'])) {
+    $loggedUser = [
+        'pseudo' => $_SESSION['LOGGED_USER'],
+    ];
+}
+?>
+
+<?php if (!isset($loggedUser)): ?>
+    <form action="login.php" method="POST">
+        <h2>Connectez-vous </h2>
+    <?php if(isset($errorMessage)) : ?>
+        <div>
+            <?php echo($errorMessage); ?>
+        </div>
+    <?php endif; ?>
+    <div>
+        <label for="pseudo">Pseudo</label>
+        <input type="text" name="pseudo" id="pseudo">
+    </div>
+    <input type="submit" value="Envoyer">
+    </form>
+<?php endif; ?>
