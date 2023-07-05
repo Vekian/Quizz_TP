80247483745080de6619399a0587a96bfcfb47e0
Mathieu Gerard <mathieu2991@gmail.com>
2023-07-05 15:57:56 +0200

    ajout de l'affichage du quizz

--- display-quizz.php
+++ display-quizz.php
@@ -1,23 +1,14 @@
 <?php
+session_start();
 include_once('connection.php');
 
-if(isset($_GET['id'])){
-$id = 1;
-$query = 'SELECT * FROM questions JOIN quizzs ON questions.id_quizz = quizzs.id WHERE quizzs.id = "'.$id.'"';
-$quizzStatement = $baseQuizz -> prepare($query);
-$quizzStatement -> execute();
-$quizzs = $quizzStatement -> fetchAll((PDO::FETCH_ASSOC));
-}
-
-$question = 0;
-foreach($quizzs as $quizz){
-    print_r($quizz['question']);
-}
 ?>
 
     <div id="answer">
     </div>
 <script>
+    let idQuizz = <?php echo($_GET['id']); ?>;
+    let name = "<?php echo($_SESSION['LOGGED_USER']) ?>";
     <?php require_once("js/display-quizz.js");?>
 </script>
 
--- js/display-quizz.js
+++ js/display-quizz.js
@@ -1,12 +1,10 @@
-
-let numberOfQuestions = 3;
-let idQuestion = 1;
-
-
-function displayAnswer(id) {
-    fetch('answer.php', {
+let array = getQuestion(idQuizz);
+console.log(array);
+function getQuestion(idQuizz){
+    let questions = [];
+    fetch('question.php', {
                             method: "POST",
-                            body: JSON.stringify(id)
+                            body: JSON.stringify(idQuizz)
                             }
     )
             .then(function(response) {
@@ -17,6 +15,39 @@ function displayAnswer(id) {
                 }
             })
             .then(function(data) {
+                for (let key in data) {
+                    let value = data[key]; 
+                    questions.push(value);
+                    questions.push(key);
+                }
+                let i = 0;
+                displayAnswer(i);
+                })
+            .catch(function(error) {
+                console.log(error);
+            });
+    
+    return questions
+}
+
+let score = 0;
+function displayAnswer(i) {
+    let idQuestion = array[i];
+    fetch('answer.php', {
+                            method: "POST",
+                            body: JSON.stringify(idQuestion)
+                            }
+    )
+            .then(function(response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Erreur lors de la requête AJAX');
+                }
+            })
+            .then(function(data) {
+                if (i <= (array.length) / 2 +1) {
+                document.getElementById('answer').innerHTML += array[i + 1] + "<br />";
                 for (let key in data) {
                     let value = data[key];
                     document.getElementById('answer').innerHTML += '<button class="button" value="'+ value + '">' + key + '</button><br />';
@@ -29,18 +60,31 @@ function displayAnswer(id) {
                     }
                     else {
                         button.setAttribute('style', 'background-color: green;')
+                        score += 10;
                     };
                 setTimeout(function (){
-                    if (id <= numberOfQuestions) {
+                    if (i <= (array.length)/ 2) {
                     document.getElementById('answer').innerHTML = "";
-                    displayAnswer(id +1);
+                    displayAnswer(i + 2);
                 }
                 }, 1000);
                 });
+            }}
+            else {
+                    document.getElementById('answer').innerHTML = "Le quizz est terminé ! <br />Votre score est de " + score;
+                    let objet = {
+                        'score': score,
+                        'name' : name,
+                        'idQuizz' : idQuizz
+                    }
+                    fetch('score.php', {
+                        method: "POST",
+                        body: JSON.stringify(objet)
+                        }); 
+                    
             }
                 })
             .catch(function(error) {
                 console.log(error);
             });}
-            displayAnswer(idQuestion);
     
