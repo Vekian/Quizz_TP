079e906430a335398cdcda2671cd41b959ca08ea
lea4203 <lea.merley@orange.fr>
2023-07-06 10:32:03 +0200

    modif header

--- header.php
+++ header.php
@@ -26,8 +26,6 @@
 
                             <a class="nav-link" href="classement-general.php">Classement</a>
 
-                            <a class="nav-link" href="classement.php">Classement</a>
-
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="compte.php">Mon compte</a>
