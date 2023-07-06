00f3a8eab46bbfee8648279339edf95f895bae06
Mathieu Gerard <mathieu2991@gmail.com>
2023-07-06 13:42:48 +0200

    modif style display quizz et ajout timer

--- display-quizz.php
+++ display-quizz.php
@@ -3,14 +3,38 @@ session_start();
 include_once('connection.php');
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/timer.css">
+</head>
+<body>
 
     <div id="answer">
     </div>
 <script>
     let idQuizz = <?php echo($_GET['id']); ?>;
     let name = "<?php echo($_SESSION['LOGGED_USER']) ?>";
+    
+    function countdown() {
+	timeLeft--;
+	document.getElementById("seconds").innerHTML = String( timeLeft );
+	if (timeLeft > 0) {
+		setTimeout(countdown, 1000);
+	}
+};
     <?php require_once("js/display-quizz.js");?>
+    
+    
+
+
 </script>
+<div id="clock">
+	<span id="seconds">10</span>
+</div>
 
-
-
+</body>
+</html>
