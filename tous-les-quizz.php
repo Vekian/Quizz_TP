7b8e3d7a0d3a4a56f3b65dc87d3abc40f6abf694
Mathieu Gerard <mathieu2991@gmail.com>
2023-07-06 10:19:46 +0200

    ajout de l'affichage de quizz

--- tous-les-quizz.php
+++ tous-les-quizz.php
@@ -1,8 +1,3 @@
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,6 +18,51 @@ include 'header.php';
         <div class="col-12">
           <h1 class="mt-5">Liste des quizzs</h1>
           <p>Voici tous les quizzs que nous proposons</p>
+          <?php
+            include('connection.php');
+
+            $query = 'SELECT * FROM quizzs JOIN questions ON quizzs.id = questions.id_quizz';
+            $quizzStatement = $baseQuizz -> prepare($query);
+            $quizzStatement -> execute();
+            $quizzs = $quizzStatement -> fetchAll(PDO::FETCH_ASSOC);
+            
+
+            $arrayUnique = uniqueValue($quizzs);
+            $arrayCount = countValue($quizzs);
+            function uniqueValue($quizzs){
+              $array = [];
+              foreach ($quizzs as $quizz) {
+              $array[$quizz['name']] = $quizz['category'];
+              }
+              return $array;
+            };
+
+            function countValue($arrays) {
+              $array1 = [];
+              foreach($arrays as $array) {
+                foreach($array as $key => $value) {
+                  if ($key == 'name') {
+                if(array_key_exists($value, $array1)) {
+                  $array1[$value] += 1;
+                }
+                else {
+                  $array1[$value] =1;
+                }}}}
+                return $array1;
+            }
+            
+            $id = 1;
+            foreach($arrayUnique as $key => $value) {
+              echo('<button onclick="window.location.href = \'display-quizz.php?id='. $id . '\';" ><div>Nom : ' . $key . '<br />');
+              echo('<div>Categorie :  ' . $value . '</div>');
+              foreach($arrayCount as $name => $count){
+                if ($name == $key){
+                  echo('<div>Ce quizz contient ' . $count . ' questions</div></div></button><br />');
+              }}
+              $id++;
+            }
+            
+          ?>
         </div>
       </div>
     </div>
