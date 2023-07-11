fdd5029a2553ed9a95dc43db957acb55a5d8035f
lea4203 <lea.merley@orange.fr>
2023-07-11 11:53:34 +0200

    modif header

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bulles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link rel="stylesheet" href="css/images-arriere-plan.css">
@@ -30,6 +31,7 @@
     <div class="animation-container">
         <div class="animation"></div>
     </div>
+    
 
     <script>
         document.getElementById("startQuizButton").addEventListener("click", function () {
